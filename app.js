d76982e8cefc0ff8fdca2eb813960f96ce008762
jacob <contacto@jacob>
2025-10-22 20:11:48 -0300

    Routing estatico

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, 'public')));
 
-    res.send('Hello world!');
-})
+app.get('/', function(req, res)
+{
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-})
+app.listen(port, function()
+{
+    console.log(`Proyecto ejecutandose en puerto: ${port}`);
+});
